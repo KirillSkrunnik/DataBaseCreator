5125434f8510c2b7865f829c93e9f5da40db605b
Kirill <zaknifen@gmail.com>
2016-02-27 11:51:53 +0200

    init

--- app/src/main/java/com/example/zaknifen/databasecreator/MainActivity.java
+++ app/src/main/java/com/example/zaknifen/databasecreator/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.zaknifen.databasecreator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.zaknifen.databasecreator.DataBase.DataBaseMaster;
+import com.example.zaknifen.databasecreator.models.User;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User user = new User();
+        user.name = "Vasya";
+
+        DataBaseMaster.getInstance(this).addUser(user);
+
+        List<User> query = DataBaseMaster.getInstance(this).getAllUsersQuery();
+        Log.d("query", "size: " + query.size());
+        List<User> raw = DataBaseMaster.getInstance(this).getAllUsersRaw();
+        Log.d("raw", "size: " + raw.size());
     }
 }
